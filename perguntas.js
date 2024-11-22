5b95eae70b84f62b506cefc59fcf9df5e13a874d
keyl1NHA <00001083559163SP@al.educacao.sp.gov.br>
2024-11-22 11:56:21 -0300

    arquivos

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é GitHub?',
+    'plataforma com banco de aramazenameto'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasilia'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz amor em Inglês?',
+    'amor em ingles é love'
+)
